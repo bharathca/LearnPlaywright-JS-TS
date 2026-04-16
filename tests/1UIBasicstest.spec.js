a1fb0af10080f1e8695bf7337708df6791db239b
Bharath Chakravarthy Aravamudhan <bharath.cra@gmail.com>
2026-04-16 22:28:36 +0530

    Section3Completed

--- tests/1UIBasicstest.spec.js
+++ tests/1UIBasicstest.spec.js
@@ -1,9 +1,24 @@
-const {test} = require('@playwright/test');
+const {test, expect} = require('@playwright/test');
 
 //await is used to make the execution in sequential order 
 //when await needs to be used then async should be used 
-test('First Playwright test',async ({browser})=>
+test('First Playwright test - initial learning',async ({browser})=>
 {
     //chrome - plugin/cookies
-    const context = browser.newContext();
+    //newContext - sort of incognito window - no history nothing - no cookies
+
+    //context -> page -> url
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    console.log(await page.title());
+    await expect(page).toHaveTitle("LoginPage Practise | Rahul Shetty Academy");
+
+});
+
+test('Page Playwright Test',async ({page})=>
+{
+    await page.goto("https://google.com");
+    console.log(await page.title());
+    await expect(page).toHaveTitle("Google");
 });
