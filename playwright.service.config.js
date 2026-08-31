1ac46681c603c2e2a680f790742a10f345b5abb9
Bharath Chakravarthy Aravamudhan <bharath.cra@gmail.com>
2026-08-31 23:25:25 +0530

    Adding .js in the service config

--- playwright.service.config.js
+++ playwright.service.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@playwright/test');
 const { createAzurePlaywrightConfig, ServiceOS } = require('@azure/playwright');
 const { DefaultAzureCredential } = require('@azure/identity');
-const config = require('./playwright.config');
+const config = require('./playwright.config.js');
 
 /* Learn more about service configuration at https://aka.ms/pww/docs/config */
 export default defineConfig(
