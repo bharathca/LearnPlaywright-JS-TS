2bff6d632d992b033fc62974a9de1d1c142e6c9e
Bharath Chakravarthy Aravamudhan <bharath.cra@gmail.com>
2026-08-31 23:20:38 +0530

    Updating the playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -3,6 +3,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   timeout: 40 * 1000,
+  retries:2,
   expect: {
     timeout: 40 * 1000
   },
